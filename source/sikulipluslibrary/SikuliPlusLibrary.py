4f3e0d696928b5912d89d6fcf5a671e94095f69f
Leonardo Sextare <leonardosextare@gmail.com>
2025-09-04 14:02:42 -0300

    primeira keyword funcional com decorador

--- source/sikulipluslibrary/SikuliPlusLibrary.py
+++ source/sikulipluslibrary/SikuliPlusLibrary.py
@@ -1,24 +1,22 @@
 from robot.libraries.BuiltIn import BuiltIn
 from SikuliLibrary import SikuliLibrary
 from robot.api.deco import library, keyword
-from .SikuliWrapper import SikuliPlusMixin
-from .mixins.ConfigMixin import ConfigMixin
-from .functions import _normalize_str_paths
+from .mixins import VisionMixin, ConfigMixin
 
 from robot.api.logger import console
 
 @library(scope="GLOBAL", version="0.1.0")
-class SikuliPlusLibrary(ConfigMixin):
+class SikuliPlusLibrary(ConfigMixin, VisionMixin):
 
     def __init__(self) -> None:
         self.ROBOT_LIBRARY_LISTENER = self  # Estudar
         self.ROBOT_LISTENER_API_VERSION = 3  # Estudar
 
-        self.global_vision_timeout: float
-        self.global_similarity: float
+        self.global_similarity: float = 0.80
 
+        self.global_vision_timeout: float = 5.0
         self.show_vision_highlight: bool
-        self.vision_highlight_timeout: int
+        self.vision_highlight_timeout: float
 
         self.show_actions_highlight: bool
         self.global_action_speed: float
@@ -26,36 +24,9 @@ class SikuliPlusLibrary(ConfigMixin):
         self.robot: BuiltIn
         self.sikuli: SikuliLibrary
 
+
     def start_suite(self, data, result):
         self._bootstrap()
 
     def close(self):
         self._shutdown()
-
-    def set_highlight(self, setting: bool, timeout: float):
-        self.show_highlight = setting
-        self.highlight_timeout = timeout
-
-    @keyword
-    def wait_screen_Contain(self, image: str, timeout: int = 5, similarity: float = 0.8, roi_image: str = ""):
-        obj_image, obj_roi_image = _normalize_str_paths(image, roi_image)
-        self.robot.run_keyword("Wait Until Screen Contain", obj_image, timeout)
-
-        # if roi_image:
-        #     self._set_roi(obj_roi_image)
-
-        # if self.show_highlight:
-        #     self._highlight(obj_image)
-
-        # self.robot.run_keyword("Wait Until Screen Contain", obj_image, timeout)
-
-        # self._reset_definitions()
-
-    @keyword
-    def image_exists(
-        self,
-        image: str,
-        timeout: int = 5,
-        similarity: float = 0.8,
-    ):
-        print("teste")
--- source/sikulipluslibrary/mixins/VisionMixin.py
+++ source/sikulipluslibrary/mixins/VisionMixin.py
@@ -1,5 +1,27 @@
+from robot.api.deco import keyword
+from SikuliPlusLibrary.decorators.similarity import _set_temporary_similarity
+from robot.libraries.BuiltIn import BuiltIn
+from SikuliLibrary import SikuliLibrary
+
 class VisionMixin:
-    def wait_until_image_appear(self): ...
+    def __init__(self) -> None:
+        self.global_similarity: float
+
+        self.global_vision_timeout: float
+        self.show_vision_highlight: bool
+        self.vision_highlight_timeout: float
+
+        self.show_actions_highlight: bool
+        self.global_action_speed: float
+
+        self.robot: BuiltIn
+        self.sikuli: SikuliLibrary
+
+    @_set_temporary_similarity
+    @keyword
+    def wait_until_image_appear(self, image: str, timeout: float):
+        self.sikuli.run_keyword("Wait Until Screen Contain", [image, timeout])
+        
     def wait_until_image_dissapear(self): ...
     def count_images(self): ...
     def image_exists(self): ...
