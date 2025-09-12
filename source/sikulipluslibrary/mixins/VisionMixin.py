3155241c6652a2c798b5b127317aeea8dbaff98b
Leonardo Sextare <leonardo.sextare@inovafarma.com.br>
2025-09-12 14:04:54 -0300

    implementa roi

--- source/sikulipluslibrary/mixins/VisionMixin.py
+++ source/sikulipluslibrary/mixins/VisionMixin.py
@@ -9,6 +9,7 @@ from SikuliPlusLibrary.decorators.timeout import override_timeout_parameter
 
 class VisionMixin:
     def __init__(self) -> None:
+        self.highlight_time: float
         self.robot: BuiltIn
         self.sikuli: SikuliLibrary
 
@@ -18,38 +19,68 @@ class VisionMixin:
     @keyword
     def wait_until_image_appear(self, image: str, timeout: float):
         self.sikuli.run_keyword("Wait Until Screen Contain", [image, timeout])
+        self.sikuli.run_keyword("Highlight", [image])
+        time.sleep(self.highlight_time)
+        self.sikuli.run_keyword("Clear All Highlights")
 
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def wait_until_image_dissapear(self, image: str, timeout: float):
+        self.sikuli.run_keyword("Highlight", [image])
         self.sikuli.run_keyword("Wait Until Screen Not Contain", [image, timeout])
+        time.sleep(self.highlight_time)
+        self.sikuli.run_keyword("Clear All Highlights")
 
+
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def count_image(self, image: str, timeout: float) -> int:
-        self.sikuli.run_keyword("Exists", [image, timeout])
+        image_exists = self.sikuli.run_keyword("Exists", [image, timeout])
 
+            
         result = self.sikuli.run_keyword("Image Count", [image])
+
+        if image_exists:
+            self.sikuli.run_keyword("Highlight", [image])
+            time.sleep(self.highlight_time)
+            self.sikuli.run_keyword("Clear All Highlights")
+            
 
         return result  # type: ignore
 
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def count_multiple_images(self, *images: str, timeout: float) -> dict[str, int]:
-        self.multiple_images_exists(*images, timeout=timeout)
+        tempo = timeout
+        self.multiple_images_exists(*images, timeout=tempo)
 
         images_counts: dict[str, int] = {}
         for img in images:
             images_counts[img] = self.sikuli.run_keyword("Image Count", [img])  # type: ignore
+
+            if images_counts[img] > 0:
+                self.sikuli.run_keyword("Highlight", [img])
+                time.sleep(self.highlight_time)
+                self.sikuli.run_keyword("Clear All Highlights")
         return images_counts
 
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def image_exists(self, image: str, timeout: float) -> bool:
         result = self.sikuli.run_keyword("Exists", [image, timeout])
         return result  # type: ignore
-    
+
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def multiple_images_exists(self, *images: str, timeout: float) -> dict[str, bool]:
         polling_inverval = 1.0
@@ -75,13 +106,17 @@ class VisionMixin:
 
             time.sleep(min(polling_inverval, deadline - now))
 
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def wait_one_of_multiple_images(self, *images: str, timeout: float) -> str:
         image_found = self.sikuli.run_keyword("Wait For Multiple Images", [timeout, 1, images, []])
         return image_found  # type: ignore
 
+    @roi_parameter
     @similarity_parameter
+    @override_timeout_parameter
     @keyword
     def wait_multiple_images(self, *images: str, timeout: float):
         polling_inverval = 1.0
@@ -105,3 +140,11 @@ class VisionMixin:
 
         remaning_images = [img for img, found in status.items() if not found]
         raise TimeoutError(f"Timed out after {timeout:.2f}s waiting for all images to be present. " f"The following images still missing: {remaning_images}.")
+
+    def _highlight(self, image, reset=True):
+        self.sikuli.run_keyword("Highlight", [image])
+
+        
+        time.sleep(self.highlight_time)
+        self.sikuli.run_keyword("Clear All Highlights")
+        
