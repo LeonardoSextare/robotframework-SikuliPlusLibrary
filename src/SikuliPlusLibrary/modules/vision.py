896839097f36e30a457654257ac2cf961c367f0b
Leonardo Sextare <leonardosextare@gmail.com>
2025-10-03 14:04:44 -0300

    implementa wait for image para teste

--- src/SikuliPlusLibrary/modules/vision.py
+++ src/SikuliPlusLibrary/modules/vision.py
@@ -1,15 +1,19 @@
 from SikuliLibrary import SikuliLibrary
+from ..mixins.vision_context import VisionContextMixin
 from typing import Optional, List
 
-class VisionModule:
+class VisionModule(VisionContextMixin):
     def __init__(self, sikuli: SikuliLibrary):
         self.sikuli = sikuli
 
-    def wait_for_image(self, image_path: str, timeout: int, similarity: float = 0.7, roi: Optional[List[int]] = None) -> Optional[str]:
-        self.sikuli.run_keyword("Wait Until Screen Contain", [image_path, timeout])
+    def wait_for_image(self, image: str, timeout: int, similarity: float, roi: Optional[List[int]]) -> Optional[str]:
+        with self._vision_context(similarity, roi=roi, timeout=timeout) as add_highlight:
+            self.sikuli.run_keyword("Wait Until Screen Contain", [image, timeout])
+            add_highlight(image)
+            return
     
-    def wait_for_any_image(self, image_paths: List[str], timeout: int = 0) -> Optional[str]:
+    def wait_for_any_image(self, images: List[str], timeout: int = 0) -> Optional[str]:
         raise NotImplementedError("This method should be implemented in the future.")
 
-    def wait_for_all_images(self, image_paths: List[str], timeout: int = 0) -> bool:
+    def wait_for_all_images(self, images: List[str], timeout: int = 0) -> bool:
         raise NotImplementedError("This method should be implemented in the future.")
